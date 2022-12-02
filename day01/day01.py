06be76137323370d65ba10e824c2ae7349010160
Paulo Sergio Jr <paulo.psw@gmail.com>
2022-12-02 00:21:18 -0300

    Refactoring day 1 + add REAME

--- day01/day01.py
+++ day01/day01.py
@@ -1,33 +1,26 @@
-example = ''
-with open('example.txt') as f:
-    example = f.read()
-
-input = ''
-with open('input.txt') as f:
-    input = f.read()
+def loadItems(inputfile):
+    inputdata = ''
+    with open(inputfile, 'r') as f:
+        inputdata = f.read()
+    
+    return [
+        list(map(int, x.splitlines())) 
+        for x 
+        in inputdata.split('\n\n')
+    ]
 
 
 def part1(input):
     """Max calories of all elves"""
-    elves = [
-        list(map(int, x.splitlines())) 
-        for x 
-        in input.split('\n\n')
-    ]
-    
+    elves = loadItems(input)
     maxcalories = max([sum(x) for x in elves])
 
     return maxcalories
 
 
 def part2(input):
-    """Sum of calores for the top 3 elves"""
-    elves = [
-        list(map(int, x.splitlines())) 
-        for x 
-        in input.split('\n\n')
-    ]
-
+    """Sum of calories from the top 3 elves"""
+    elves = loadItems(input)
     calories = [sum(x) for x in elves]
     calories.sort()
     top3 = calories[-3:]
@@ -36,8 +29,8 @@ def part2(input):
 
 
 if __name__ == '__main__':
-    print(f'Part 1: {part1(input)}')
-    assert part1(example) == 24000
+    print(f'Part 1: {part1("input.txt")}')
+    assert part1("example.txt") == 24000
 
-    print(f'Part 2: {part2(input)}')
-    assert part2(example) == 45000
+    print(f'Part 2: {part2("input.txt")}')
+    assert part2("example.txt") == 45000
