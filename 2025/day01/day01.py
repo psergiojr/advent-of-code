2a761a6dc35b32aa73f3454b32f79b0165336adb
Paulo Sergio Junior <paulo.psw@gmail.com>
2025-12-02 23:45:04 -0300

    AoC 2025 - Day 01 - Part 2

--- 2025/day01/day01.py
+++ 2025/day01/day01.py
@@ -1,27 +1,72 @@
-def part1(filename: str) -> int:
+import unittest
+
+
+def parse_input(filename: str) -> list[tuple[str, int]]:
     data = []
-    angle: int = 50
-    zero_counter: int = 0
 
     with open(filename) as f:
         data = [line.strip() for line in f.readlines()]
     
-    for spin in data:
-        if spin.startswith('L'):
-            angle = (angle - int(spin[1:])) % 100
-        elif spin.startswith('R'):
-            angle = (angle + int(spin[1:])) % 100
+    rotations = [
+        ( line[0], int(line[1:]) )
+        for line in data
+    ]
+    return rotations
 
-        if angle == 0:
-            zero_counter += 1
+
+def part1(filename: str) -> int:
+    return count_zeroes(filename)
+
+
+def part2(filename: str) -> int:
+    return count_zeroes(filename, in_between=True)
+
+
+def count_zeroes(filename: str, in_between: bool = False):
+    dial: int = 50
+    zeroes_at_end: int = 0
+    zeroes_in_between: int = 0
+
+    rotations = parse_input(filename)
+
+    for (direction, value) in rotations:
+        zeroes_in_between += value // 100 # full spins
+        remainder = value % 100
+
+        if direction == 'L':
+            if dial != 0 and dial - remainder < 0:
+                zeroes_in_between += 1
+            dial = (dial - remainder) % 100
+        elif direction == 'R':
+            if dial + remainder > 100:
+                zeroes_in_between += 1
+            dial = (dial + remainder) % 100
+
+        if dial == 0:
+            zeroes_at_end += 1
     
-    return zero_counter
+    if in_between:
+        return zeroes_at_end + zeroes_in_between
+    else:
+        return zeroes_at_end
+
+
+class TestDay01(unittest.TestCase):
+    def test_part1_example(self):
+        self.assertEqual(part1("example.txt"), 3)
+    
+    def test_part1_input(self):
+        self.assertEqual(part1("input.txt"), 964)
+    
+    def test_part2_example(self):
+        self.assertEqual(part2("example.txt"), 6)
+    
+    def test_part2_input(self):
+        self.assertEqual(part2("input.txt"), 5872)
 
 
 if __name__ == "__main__":
-    result = part1("example.txt")
-    print(f"Part 1 (example): {result}")
-    assert result == 3
+    print("Part 1:", part1("input.txt"))
+    print("Part 2:", part2("input.txt"))
 
-    result = part1("input.txt")
-    print(f"Part 1 (input): {result}")
+    unittest.main()
